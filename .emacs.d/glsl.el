6eb9ad553b75622ed7218992bc1f3511fce3aeaa
TheVaffel <hkon20@hotmail.com>
2022-11-07 13:41:54 +0100

    Add another GLSL hook

--- .emacs.d/glsl.el
+++ .emacs.d/glsl.el
@@ -4,3 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
+
+(add-hook 'glsl-mode-hook (lambda ()
+                            (setq auto-save-default nil)
+                            (setq create-lockfiles nil)))
