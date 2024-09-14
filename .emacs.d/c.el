0759b0f5a32bf4429ff998dc324db8331e18101f
Håkon Flatval <hkon20@hotmail.com>
2024-09-14 11:32:12 +0200

    Let C basic offset be 4

--- .emacs.d/c.el
+++ .emacs.d/c.el
@@ -1,6 +1,7 @@
 
 ;; Custom C stuff
 
+(setq c-basic-offset 4)
 (add-hook 'c-mode-hook (lambda () ""
                          (setq c-basic-offset 2)
                          (set-indent-tabs-mode nil)))
