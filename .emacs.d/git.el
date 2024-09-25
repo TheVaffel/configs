ed8601fbc65e3e17677a2cec0c9568455d13d3c7
TheVaffel <hkon20@hotmail.com>
2024-09-25 15:31:01 +0200

    add git config

---
+++ .emacs.d/git.el
@@ -0,0 +1,7 @@
+
+
+;; (use-package magit-section)
+(use-package magit)
+;; (magit-add-section-hook 'magit-status-sections-hook #'magit-insert-ignored-files nil t)
+;; (require 'magit-gh-pulls)
+;; (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
